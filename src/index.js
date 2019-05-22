026bd7988b8632520b393125e26be2e02bb29a9a
yidih <378008136@qq.com>
2019-05-22 15:34:11 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Switch, Route, HashRouter, Redirect } from 'react-router-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import loadable from '@loadable/component';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Loading = (
+  <div>
+    Loading...
+  </div>
+)
+
+const getComponent = (nextState, cb) => {
+  const { page } = nextState.match.params;
+  const Component = loadable(
+    () => import(`./pages/${page}`),
+    { fallback: Loading }
+  );
+  return <Component {...nextState}/>;
+};
+
+ReactDOM.render(
+  <HashRouter>
+    <Switch>
+      <Route path={'/:page'} component={getComponent} />
+      <Redirect to="/home" />
+    </Switch>
+  </HashRouter>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
